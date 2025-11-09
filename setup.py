ee81181b5c061d487a8187400fcc7adbd732228e
Mukesh <chaharmukesh518@gmail.com>
2025-11-09 19:00:36 +0530

    Project Structure Ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mukesh Kumar",
+    author_email="chaharmukesh518@gmail.com",
+    packages=find_packages()
+)
